de4f17455641a5495093910a93d55e440a174f54
Scott Yang <67733409+scott-yj-yang@users.noreply.github.com>
2024-05-08 12:52:18 -0700

    fix reference_rel_joints's size

--- environments.py
+++ environments.py
@@ -437,8 +437,8 @@ class RodentSingleClipTrack(PipelineEnv):
         """Observation of the reference joints relative to walker."""
         time_steps = frame + jp.arange(self._ref_traj_length)
 
-        qpos_ref = ref_traj.joints[frame, :]
-        diff = qpos_ref[time_steps] - data.qpos[7:]  # not sure if correct?
+        qpos_ref = ref_traj.joints[time_steps, :]
+        diff = qpos_ref - data.qpos[7:]  # hard coded 7:?
 
         # what would be a  equivalents of this?
         # return diff[:, self._walker.mocap_to_observable_joint_order].flatten()
