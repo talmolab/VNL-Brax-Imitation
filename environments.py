6abf51f458c3508bfff02e9cddcade841ab5d5d4
Kevin Bian <kaiwenbian107@gmail.com>
2024-05-16 16:54:26 -0700

    smaller error threshold

--- environments.py
+++ environments.py
@@ -104,7 +104,7 @@ class RodentSingleClipTrack(PipelineEnv):
       clip_length: int=250,
       episode_length: int=150,
       ref_traj_length: int=5,
-      termination_threshold: float=0.3, # strct error threshold
+      termination_threshold: float=0.021, # strct error threshold
       body_error_multiplier: float=1.0,
       **kwargs,
   ):
