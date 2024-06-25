2f0a1eacf49ed3e6623de1f71fe05c861cf93fff
Kevin Bian <kaiwenbian107@gmail.com>
2024-06-25 13:57:26 -0700

    fixed termination index

--- envs/rodent.py
+++ envs/rodent.py
@@ -284,7 +284,7 @@ class RodentTracking(PipelineEnv):
         error_joints = jp.linalg.norm((target_joints - data_c.qpos[7:]), ord=1)
         target_bodies = self._ref_traj.body_positions[state.info["cur_frame"], :]
         error_bodies = jp.linalg.norm(
-            (target_bodies - data_c.xpos[:,self._body_idxs]), ord=1 #TODO: this order index correct?
+            (target_bodies - data_c.xpos[self._body_idxs]), ord=1 #TODO: this order index correct?
         )
         error = 0.5 * self._body_error_multiplier * error_bodies + 0.5 * error_joints
         termination_error = 1 - (error / self._termination_threshold) # low threshold, easier to terminate, more sensitive
