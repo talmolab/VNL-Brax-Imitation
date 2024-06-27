78bcf681efa4c19711a3f6bf92034fe557b4f4d8
Kevin Bian <kaiwenbian107@gmail.com>
2024-06-27 11:43:06 -0700

    cleaned code

---
+++ envs/rodent_vcl.py
@@ -0,0 +1,521 @@
+import jax
+from jax import numpy as jp
+from typing import Any
+
+from brax.envs.base import PipelineEnv, State
+from brax.io import mjcf as mjcf_brax
+from brax.base import Motion, Transform
+from brax.mjx.pipeline import _reformat_contact
+from brax.base import Motion, Transform
+from brax.mjx.pipeline import _reformat_contact
+
+from dm_control import mjcf
+from dm_control.locomotion.walkers import rescale
+
+import mujoco
+from mujoco import mjx
+import numpy as np
+import h5py
+import os
+from mujoco.mjx._src.dataclasses import PyTreeNode
+from walker import Rat
+import pickle
+
+
+class RodentTracking(PipelineEnv):
+    def __init__(
+        self,
+        params,
+        healthy_z_range=(0.05, 0.5),
+        reset_noise_scale=1e-3,
+        clip_length: int = 250,
+        sub_clip_length: int = 10,
+        ref_traj_length: int = 5,
+        termination_threshold: float = 5,
+        body_error_multiplier: float = 1.0,
+        explore_time: int = 20,
+        curriculum_max_time: int = 50,
+        **kwargs,
+    ):
+        # body_idxs => walker_bodies => body_positions
+        root = mjcf.from_path("./assets/rodent.xml")
+
+        # TODO: replace this rescale with jax version (from james cotton BodyModels)
+        rescale.rescale_subtree(
+            root,
+            params["scale_factor"],
+            params["scale_factor"],
+        )
+        mj_model = mjcf.Physics.from_mjcf_model(root).model.ptr
+
+        mj_model.opt.cone = mujoco.mjtCone.mjCONE_PYRAMIDAL
+
+        mj_model.opt.solver = {
+            "cg": mujoco.mjtSolver.mjSOL_CG,
+            "newton": mujoco.mjtSolver.mjSOL_NEWTON,
+        }[params["solver"].lower()]
+        mj_model.opt.iterations = params["iterations"]
+        mj_model.opt.ls_iterations = params["ls_iterations"]
+        mj_model.opt.jacobian = 0  # dense
+
+        self._end_eff_idx = jp.array(
+            [
+                mujoco.mj_name2id(mj_model, mujoco.mju_str2Type("body"), body)
+                for body in params["end_eff_names"]
+            ]
+        )
+
+        self._body_idxs = jp.array(
+            [
+                mujoco.mj_name2id(mj_model, mujoco.mju_str2Type("body"), body)
+                for body in params["walker_body_names"]
+            ]
+        )
+
+        self._joint_idxs = jp.array(
+            [
+                mujoco.mj_name2id(mj_model, mujoco.mju_str2Type("joint"), joint)
+                for joint in params["joint_names"]
+            ]
+        )
+
+        sys = mjcf_brax.load_model(mj_model)
+
+        physics_steps_per_control_step = 5
+
+        kwargs["n_frames"] = kwargs.get("n_frames", physics_steps_per_control_step)
+        kwargs["backend"] = "mjx"
+
+        super().__init__(sys, **kwargs)
+
+        self._healthy_z_range = healthy_z_range
+        self._explore_time = explore_time
+        self._reset_noise_scale = reset_noise_scale
+        self._termination_threshold = termination_threshold
+        self._body_error_multiplier = body_error_multiplier
+        self._clip_length = clip_length
+        self._sub_clip_length = sub_clip_length
+        self._ref_traj_length = ref_traj_length
+        self._termination_threshold = termination_threshold
+        self._body_error_multiplier = body_error_multiplier
+        self._curriculum_max_time = curriculum_max_time
+
+        with open(params["clip_path"], "rb") as f:
+            self._ref_traj = pickle.load(f)
+
+        if self._sub_clip_length > self._clip_length:
+            raise ValueError("sub clip length cannot be greater than clip_length!")
+
+    def reset(self, rng) -> State:
+        """
+        Resets the environment to an initial state.
+        TODO: add a small amt of noise (qpos + epsilon) for randomization purposes
+        """
+        start_frame = jax.random.randint(
+            rng,
+            (),
+            0,
+            self._clip_length - self._sub_clip_length - self._ref_traj_length,
+        )
+        # start_frame = 0
+
+        old, rng = jax.random.split(rng)
+        noise = self._reset_noise_scale * jax.random.normal(rng, shape=(self.sys.nq,))
+
+        qpos = jp.hstack(
+            [
+                self._ref_traj.position[start_frame, :],
+                self._ref_traj.quaternion[start_frame, :],
+                self._ref_traj.joints[start_frame, :],
+            ]
+        )
+        qvel = jp.hstack(
+            [
+                self._ref_traj.velocity[start_frame, :],
+                self._ref_traj.angular_velocity[start_frame, :],
+                self._ref_traj.joints_velocity[start_frame, :],
+            ]
+        )
+        data = self.pipeline_init(qpos + noise, qvel)
+        traj = self._get_traj(data, start_frame)
+
+        info = {
+            "cur_frame": start_frame,
+            "traj": traj,
+            "first_reset": 0,
+            "curriculum_length": 0,
+            "sub_clip_length": self._sub_clip_length,
+        }
+        obs = self._get_obs(data, jp.zeros(self.sys.nu), info)
+        reward, done, zero = jp.zeros(3)
+        metrics = {
+            "rcom": zero,
+            "rvel": zero,
+            "rtrunk": zero,
+            "rquat": zero,
+            "ract": zero,
+            "rapp": zero,
+            "termination_error": zero,
+        }
+
+        state = State(data, obs, reward, done, metrics, info)
+        termination_error = self._calculate_termination(state)
+        info["termination_error"] = termination_error
+        # if termination_error > 1e-1:
+        #   raise ValueError(('The termination exceeds 1e-2 at initialization. '
+        #                     'This is likely due to a proto/walker mismatch.'))
+        state = state.replace(info=info)
+
+        return state
+
+    def reset_to_frame(self, start_frame) -> State:
+        """
+        Resets the environment to the initial frame
+        """
+
+        qpos = jp.hstack(
+            [
+                self._ref_traj.position[start_frame, :],
+                self._ref_traj.quaternion[start_frame, :],
+                self._ref_traj.joints[start_frame, :],
+            ]
+        )
+        qvel = jp.hstack(
+            [
+                self._ref_traj.velocity[start_frame, :],
+                self._ref_traj.angular_velocity[start_frame, :],
+                self._ref_traj.joints_velocity[start_frame, :],
+            ]
+        )
+        data = self.pipeline_init(qpos, qvel)
+        traj = self._get_traj(data, start_frame)
+
+        info = {
+            "cur_frame": start_frame,
+            "traj": traj,
+            "first_reset": 0,
+            "curriculum_length": 0,
+            "sub_clip_length": self._sub_clip_length,
+        }
+        obs = self._get_obs(data, jp.zeros(self.sys.nu), info)
+        reward, done, zero = jp.zeros(3)
+        metrics = {
+            "rcom": zero,
+            "rvel": zero,
+            "rtrunk": zero,
+            "rquat": zero,
+            "ract": zero,
+            "rapp": zero,
+            "termination_error": zero,
+        }
+
+        state = State(data, obs, reward, done, metrics, info)
+        termination_error = self._calculate_termination(state)
+        info["termination_error"] = termination_error
+        # if termination_error > 1e-1:
+        #   raise ValueError(('The termination exceeds 1e-2 at initialization. '
+        #                     'This is likely due to a proto/walker mismatch.'))
+        state = state.replace(info=info)
+
+        return state
+
+    def step(self, state: State, action: jp.ndarray) -> State:
+        """Runs one timestep of the environment's dynamics."""
+        data0 = state.pipeline_state
+        data = self.pipeline_step(data0, action)
+
+        info = state.info.copy()
+        info["cur_frame"] += 1
+        info["first_reset"] += 1
+        info["curriculum_length"] += 1
+
+        obs = self._get_obs(data, action, state.info)
+        traj = self._get_traj(data, info["cur_frame"])
+
+        rcom, rvel, rtrunk, rquat, ract, rapp, is_healthy = self._calculate_reward(
+            state, data
+        )
+        rcom *= 0.01
+        rvel *= 0.01
+        rapp *= 0.01
+        rtrunk *= 0.01
+        rtrunk += 0
+        rquat *= 0.01
+        ract *= 0.0001
+
+        total_reward = rcom + rvel + rtrunk + rquat + ract + rapp
+
+        # increment frame tracker and update termination error
+        info["termination_error"] = rtrunk
+        info["traj"] = traj
+
+        sub_clip_length = jp.where(
+            (info["curriculum_length"] % self._curriculum_max_time == 0)
+            | (info["termination_error"] >= 0.25),
+            info["sub_clip_length"] * 2,
+            info["sub_clip_length"],
+        )  # values from data
+
+        self._sub_clip_length = sub_clip_length
+
+        done = jp.where((rtrunk < 0), jp.array(1, float), jp.array(0, float))
+
+        # done = jp.where(
+        #     (info["first_reset"] <= self._explore_time), jp.array(0, float), done
+        # )
+
+        done = jp.max(jp.array([1.0 - is_healthy, done]))
+
+        reward = jp.nan_to_num(total_reward)
+        obs = jp.nan_to_num(obs)
+
+        from jax.flatten_util import ravel_pytree
+
+        flattened_vals, _ = ravel_pytree(data)
+        num_nans = jp.sum(jp.isnan(flattened_vals))
+        done = jp.where(num_nans > 0, 1.0, done)
+
+        state.metrics.update(
+            rcom=rcom,
+            rvel=rvel,
+            rapp=rapp,
+            rquat=rquat,
+            rtrunk=rtrunk,
+            ract=ract,
+            termination_error=rtrunk,
+        )
+
+        return state.replace(
+            pipeline_state=data, obs=obs, reward=reward, done=done, info=info
+        )
+
+    def _calculate_termination(self, state) -> float:
+        """
+        calculates whether the termination condition is met
+        Args:
+            state (_type_): _description_
+            ref (_type_): reference trajectory
+        Returns:
+            bool: _description_
+        """
+        data_c = state.pipeline_state
+
+        target_joints = self._ref_traj.joints[state.info["cur_frame"], :]
+        error_joints = jp.linalg.norm((target_joints - data_c.qpos[7:]), ord=1)
+
+        target_bodies = self._ref_traj.body_positions[state.info["cur_frame"], :]
+        error_bodies = jp.linalg.norm(
+            (target_bodies - data_c.xpos[self._body_idxs]), ord=1
+        )
+        error = 0.5 * self._body_error_multiplier * error_bodies + 0.5 * error_joints
+        termination_error = 1 - (
+            error / self._termination_threshold
+        )  # low threshold, easier to terminate, more sensitive
+
+        return termination_error
+
+    def _calculate_reward(self, state, data_c):
+        """
+        calculates the tracking reward:
+        1. rcom: comparing center of mass
+        2. rvel: comparing joint angle velcoity
+        3. rquat: comprae joint angle position
+        4. ract: compare control force
+        5. rapp: compare end effector appendage positions
+        Args:
+            state (_type_): _description_
+        """
+        # location using com (dim=3)
+        com_c = data_c.subtree_com[1]
+        com_ref = self._ref_traj.center_of_mass[state.info["cur_frame"], :]
+        rcom = jp.exp(-100 * (jp.linalg.norm(com_c - (com_ref))))
+
+        # joint angle velocity
+        qvel_c = data_c.qvel
+        qvel_ref = jp.hstack(
+            [
+                self._ref_traj.velocity[state.info["cur_frame"], :],
+                self._ref_traj.angular_velocity[state.info["cur_frame"], :],
+                self._ref_traj.joints_velocity[state.info["cur_frame"], :],
+            ]
+        )
+        rvel = jp.exp(-0.1 * (jp.linalg.norm(qvel_c - qvel_ref)))
+
+        # rtrunk = termination error
+        rtrunk = self._calculate_termination(state)
+
+        quat_c = data_c.qpos[3:7]
+        quat_ref = self._ref_traj.quaternion[state.info["cur_frame"], :]
+        # use bounded_quat_dist from dmcontrol
+        rquat = jp.exp(-2 * (jp.linalg.norm(self._bounded_quat_dist(quat_c, quat_ref))))
+
+        # control force from actions
+        ract = -0.015 * jp.mean(jp.square(data_c.qfrc_actuator))
+
+        # end effector positions
+        app_c = data_c.xpos[self._end_eff_idx].flatten()
+        app_ref = self._ref_traj.end_effectors[state.info["cur_frame"], :].flatten()
+
+        rapp = jp.exp(-400 * (jp.linalg.norm(app_c - app_ref)))
+
+        is_healthy = jp.where(data_c.q[2] < self._healthy_z_range[0], 0.0, 1.0)
+        is_healthy = jp.where(data_c.q[2] > self._healthy_z_range[1], 0.0, is_healthy)
+        return rcom, rvel, rtrunk, rquat, ract, rapp, is_healthy
+
+    def _get_obs(self, data: mjx.Data, action: jp.ndarray, info) -> jp.ndarray:
+        """
+        Get env state obs only
+        """
+
+        # Get the relevant slice of the ref_traj
+        # TODO: can just use jax.lax.slice
+        def f(x):
+            if len(x.shape) != 1:
+                return jax.lax.dynamic_slice_in_dim(
+                    x,
+                    info["cur_frame"] + 1,
+                    self._ref_traj_length,
+                )
+            return jp.array([])
+
+        # TODO: end effectors pos and appendages pos are two different features?
+        end_effectors = data.xpos[self._end_eff_idx].flatten()
+
+        return jp.concatenate(
+            [
+                data.qpos,
+                data.qvel,
+                data.qfrc_actuator,  # Actuator force <==> joint torque sensor?
+                end_effectors,
+            ]
+        )
+
+    def _get_traj(self, data: mjx.Data, cur_frame: int) -> jp.ndarray:
+        """
+        Gets reference trajectory obs for separate pathway, storage in the info section of state
+        """
+
+        # Get the relevant slice of the ref_traj
+        def f(x):
+            if len(x.shape) != 1:
+                return jax.lax.dynamic_slice_in_dim(
+                    x,
+                    cur_frame + 1,
+                    self._ref_traj_length,
+                )
+            return jp.array([])
+
+        ref_traj = jax.tree_util.tree_map(f, self._ref_traj)
+        reference_appendages = self.get_reference_appendages_pos(ref_traj)
+        reference_rel_bodies_pos_local = self.get_reference_rel_bodies_pos_local(
+            data, ref_traj
+        )
+        reference_rel_bodies_pos_global = self.get_reference_rel_bodies_pos_global(
+            data, ref_traj
+        )
+        reference_rel_root_pos_local = self.get_reference_rel_root_pos_local(
+            data, ref_traj
+        )
+        reference_rel_joints = self.get_reference_rel_joints(data, ref_traj)
+
+        return jp.concatenate(
+            [
+                reference_appendages,
+                reference_rel_bodies_pos_local,
+                reference_rel_bodies_pos_global,
+                reference_rel_root_pos_local,
+                reference_rel_joints,
+            ]
+        )
+
+    def global_vector_to_local_frame(self, data, vec_in_world_frame):
+        """Linearly transforms a world-frame vector into entity's local frame.
+
+        Note that this function does not perform an affine transformation of the
+        vector. In other words, the input vector is assumed to be specified with
+        respect to the same origin as this entity's local frame. This function
+        can also be applied to matrices whose innermost dimensions are either 2 or
+        3. In this case, a matrix with the same leading dimensions is returned
+        where the innermost vectors are replaced by their values computed in the
+        local frame.
+
+        Returns the resulting vector, converting to ego-centric frame
+        """
+        # TODO: confirm index
+        xmat = jp.reshape(data.xmat[1], (3, 3))
+        # The ordering of the np.dot is such that the transformation holds for any
+        # matrix whose final dimensions are (2,) or (3,).
+
+        # Each element in xmat is a 3x3 matrix that describes the rotation of a body relative to the global coordinate frame, so
+        # use rotation matrix to dot the vectors in the world frame, transform basis
+        if vec_in_world_frame.shape[-1] == 2:
+            return jp.dot(vec_in_world_frame, xmat[:2, :2])
+        elif vec_in_world_frame.shape[-1] == 3:
+            return jp.dot(vec_in_world_frame, xmat)
+        else:
+            raise ValueError(
+                "`vec_in_world_frame` should have shape with final "
+                "dimension 2 or 3: got {}".format(vec_in_world_frame.shape)
+            )
+
+    def get_reference_rel_bodies_pos_local(self, data, ref_traj):
+        """Observation of the reference bodies relative to walker in local frame."""
+
+        # self._walker_features['body_positions'] is the equivalent of
+        # the ref traj 'body_positions' feature but calculated for the current walker state
+
+        xpos_broadcast = jp.broadcast_to(
+            data.xpos[self._body_idxs], ref_traj.body_positions.shape
+        )
+        obs = self.global_vector_to_local_frame(
+            data, ref_traj.body_positions - xpos_broadcast
+        )[:, self._body_idxs]
+        return jp.concatenate([o.flatten() for o in obs])
+
+    def get_reference_rel_bodies_pos_global(self, data, ref_traj):
+        """Observation of the reference bodies relative to walker, global frame directly"""
+        xpos_broadcast = jp.broadcast_to(
+            data.xpos[self._body_idxs], ref_traj.body_positions.shape
+        )
+        diff = (ref_traj.body_positions - xpos_broadcast)[:, self._body_idxs]
+
+        return diff.flatten()
+
+    def get_reference_rel_root_pos_local(self, data, ref_traj):
+        """Reference position relative to current root position in root frame."""
+        thing = ref_traj.position - data.qpos[:3]
+        obs = self.global_vector_to_local_frame(data, thing)
+        return jp.concatenate([o.flatten() for o in obs])
+
+    def get_reference_rel_joints(self, data, ref_traj):
+        """Observation of the reference joints relative to walker."""
+        diff = (ref_traj.joints - data.qpos[7:])[:, self._joint_idxs]
+
+        # diff = (qpos_ref - data.qpos[7:])[:,self._joint_idxs]
+        return diff.flatten()
+
+    def get_reference_appendages_pos(self, ref_traj):
+        """Reference appendage positions in reference frame, not relative."""
+        return ref_traj.appendages.flatten()
+
+    def _bounded_quat_dist(self, source: np.ndarray, target: np.ndarray) -> np.ndarray:
+        """Computes a quaternion distance limiting the difference to a max of pi/2.
+
+        This function supports an arbitrary number of batch dimensions, B.
+
+        Args:
+          source: a quaternion, shape (B, 4).
+          target: another quaternion, shape (B, 4).
+
+        Returns:
+          Quaternion distance, shape (B, 1).
+        """
+        source /= jp.linalg.norm(source, axis=-1, keepdims=True)
+        target /= jp.linalg.norm(target, axis=-1, keepdims=True)
+        # "Distance" in interval [-1, 1].
+        dist = 2 * jp.einsum("...i,...i", source, target) ** 2 - 1
+        # Clip at 1 to avoid occasional machine epsilon leak beyond 1.
+        dist = jp.minimum(1.0, dist)
+        # Divide by 2 and add an axis to ensure consistency with expected return
+        # shape and magnitude.
+        return 0.5 * jp.arccos(dist)[..., np.newaxis]
---
+++ ppo_imitation/latent_intention_policy_network.py
@@ -0,0 +1,150 @@
+import dataclasses
+from typing import Any, Callable, Sequence, Tuple
+import warnings
+
+from brax.training import networks
+from brax.training import types
+from brax.training import distribution
+from brax.training.networks import MLP
+import brax.training.agents.ppo.networks as ppo_networks
+from brax.training.types import PRNGKey
+
+import jax
+import jax.numpy as jnp
+from jax import random
+
+import flax
+from flax import linen as nn
+
+
+# Define the KL divergence loss
+def kl_divergence(mean, logvar):
+    return -0.5 * jnp.sum(1 + logvar - jnp.square(mean) - jnp.exp(logvar))
+
+class Encoder(nn.Module):
+    """outputs in the form of distributions in latent space"""
+
+    layer_sizes: Sequence[int]
+    latents: int  # intention size
+    activation: networks.ActivationFn = nn.relu
+    kernel_init: networks.Initializer = jax.nn.initializers.lecun_uniform()
+    bias: bool = True
+
+    @nn.compact
+    def __call__(self, x: jnp.ndarray):
+        # For each layer in the sequence
+        for i, hidden_size in enumerate(self.layer_sizes):
+            x = nn.Dense(
+                hidden_size,
+                name=f"hidden_{i}",
+                kernel_init=self.kernel_init,
+                use_bias=self.bias,
+            )(x)
+            x = self.activation(x)
+            x = nn.LayerNorm()(x)
+            
+
+        mean_x = nn.Dense(self.latents, name="fc2_mean")(x)
+        logvar_x = nn.Dense(self.latents, name="fc2_logvar")(x)
+        return mean_x, logvar_x
+
+
+class Decoder(nn.Module):
+    """decode with action output"""
+
+    layer_sizes: Sequence[int]
+    activation: networks.ActivationFn = nn.relu
+    kernel_init: networks.Initializer = jax.nn.initializers.lecun_uniform()
+    activate_final: bool = False
+    bias: bool = True
+
+    @nn.compact
+    def __call__(self, x: jnp.ndarray):
+        for i, hidden_size in enumerate(self.layer_sizes):
+            x = nn.Dense(
+                hidden_size,
+                name=f"hidden_{i}",
+                kernel_init=self.kernel_init,
+                use_bias=self.bias,
+            )(x)
+            if i != len(self.layer_sizes) - 1 or self.activate_final:
+                x = self.activation(x)
+                x = nn.LayerNorm()(x)
+        return x
+
+
+def reparameterize(rng, mean, logvar):
+    std = jnp.exp(0.5 * logvar)
+    eps = random.normal(rng, logvar.shape)
+    return mean + eps * std
+
+
+class IntentionNetwork(nn.Module):
+    """Full VAE model, encode -> decode with sampled actions"""
+
+    encoder_layers: Sequence[int]
+    decoder_layers: Sequence[int]
+    latents: int = 60
+    batch_size: int = 1 # need values
+
+    def setup(self):
+
+        key = jax.random.PRNGKey(100)
+        _, dummy_rng = jax.random.split(key, 2)
+
+        self.encoder = Encoder(layer_sizes=self.encoder_layers, latents=self.latents)
+        self.decoder = Decoder(layer_sizes=self.decoder_layers)
+
+        #TODO: consider adding prev_latent/latent ratio logging?
+        dummy_mean = jnp.zeros((self.batch_size, self.latents))
+        dummy_logvar = jnp.zeros((self.batch_size, self.latents))
+        self.prev_latent = reparameterize(dummy_rng, dummy_mean, dummy_logvar)
+
+    def __call__(self, traj, obs, key):
+        '''
+        args:
+        separate trajectory input + observation input
+        '''
+        _, encoder_rng = jax.random.split(key, 2)
+
+        print(traj.shape)
+
+        # construct the intention network
+        concat_traj = jnp.concat([traj, self.prev_latent], axis=0)
+        intention_mean, intention_logvar = self.encoder(concat_traj)
+        z = reparameterize(encoder_rng, intention_mean, intention_logvar)
+        self.prev_latent = z
+        action = self.decoder(jnp.concatenate([z, obs], axis=-1))
+
+        return action, intention_mean, intention_logvar
+
+
+def make_intention_policy(
+    param_size: int,
+    latent_size: int,
+    obs_size: int,
+    traj_size: int,  # the size of the intended trajectory
+    preprocess_observations_fn: types.PreprocessObservationFn = types.identity_observation_preprocessor,
+    encoder_layer_sizes: Sequence[int] = (1024, 1024),
+    decoder_layer_sizes: Sequence[int] = (1024, 1024),
+) -> IntentionNetwork:
+    """Creates an intention policy network."""
+
+    policy_module = IntentionNetwork(
+        encoder_layers=list(encoder_layer_sizes) + [latent_size], # added in the size of latent layer
+        decoder_layers=list(decoder_layer_sizes) + [param_size],
+        latents=latent_size,
+        batch_size=traj_size.shape[:-1] # get all prvious vmap dimension, the traj should be flattened
+    )
+
+    def apply(processor_params, policy_params, traj, obs, key):
+        obs = preprocess_observations_fn(obs, processor_params)
+        return policy_module.apply(policy_params, traj=traj, obs=obs, key=key)
+
+    dummy_obs = jnp.zeros((1, obs_size))
+    dummy_traj = jnp.zeros((1, traj_size))
+    dummy_key = jax.random.PRNGKey(0)
+
+    return networks.FeedForwardNetwork(
+        init=lambda key: policy_module.init(key, dummy_traj, dummy_obs, dummy_key), apply=apply
+    )
